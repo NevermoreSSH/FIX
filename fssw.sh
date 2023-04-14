d48da6975f69fbb6b8cdcb1e1495f23741e4868b
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-04-14 16:22:13 +0800

    Update fssw.sh

--- fssw.sh
+++ fssw.sh
@@ -1,5 +1,5 @@
 wget -q -O /etc/systemd/system/ws-tls.service "https://raw.githubusercontent.com/NevermoreSSH/hop/main/websocket/ws-tls.service" && chmod +x /etc/systemd/system/ws-tls.service
 wget -q -O /usr/local/bin/ws-tls "https://raw.githubusercontent.com/NevermoreSSH/hop/main/websocket/ws-tls" && chmod +x /usr/local/bin/ws-tls
-wget -q -O /etc/default/sslh "https://raw.githubusercontent.com/NevermoreSSH/FIX/main/sslh" && chmod +x /etc/default/sslh
+wget -q -O /etc/default/sslh "https://raw.githubusercontent.com/NevermoreSSH/FIX/main/sslh.sh" && chmod +x /etc/default/sslh
 wget -q -O /etc/stunnel5/stunnel5.conf "https://raw.githubusercontent.com/NevermoreSSH/FIX/main/stunnel5.conf" && chmod +x /etc/stunnel5/stunnel5.conf
 restart
