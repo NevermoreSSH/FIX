72f234706bbd428a33dbfe43bf92f8b55704c8b5
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-04-19 07:43:29 +0800

    Add files via upload

--- menu.sh
+++ menu.sh
@@ -30,8 +30,18 @@ uptime="$(uptime -p | cut -d " " -f 2-10)"
 tram=$(free -m | awk 'NR==2 {print $2}')
 uram=$(free -m | awk 'NR==2 {print $3}')
 # Total BANDWIDTH
+dtoday="$(vnstat -i eth0 | grep "today" | awk '{print $2" "substr ($3, 1, 1)}')"
+utoday="$(vnstat -i eth0 | grep "today" | awk '{print $5" "substr ($6, 1, 1)}')"
 ttoday="$(vnstat -i eth0 | grep "today" | awk '{print $8" "substr ($9, 1, 1)}')"
+#Download/Upload yesterday
+dyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $2" "substr ($3, 1, 1)}')"
+uyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $5" "substr ($6, 1, 1)}')"
+tyest="$(vnstat -i eth0 | grep "yesterday" | awk '{print $8" "substr ($9, 1, 1)}')"
+#Download/Upload current month
+dmon="$(vnstat -i eth0 -m | grep "$(date +"%b '%y")" | awk '{print $3" "substr ($4, 1, 1)}')"
+umon="$(vnstat -i eth0 -m | grep "$(date +"%b '%y")" | awk '{print $6" "substr ($7, 1, 1)}')"
 tmon="$(vnstat -i eth0 -m | grep "$(date +"%b '%y")" | awk '{print $9" "substr ($10, 1, 1)}')"
+
 echo ""
 echo -e "$y                        MAIN MENU $wh"
 echo -e "$y                Script Mod By NevermoreSSH $wh"
@@ -48,8 +58,10 @@ echo -e "  ${RB}♦️${NC} ${YB}IPVPS   :  $IPVPS ${NC} "
 echo -e "${BB}————————————————————————————————————————————————————————${NC}"
 echo -e "                  ${WB} ♦️ ️Total Bandwidth ♦️ ${NC}            "
 echo -e "${BB}————————————————————————————————————————————————————————${NC}"
-echo -e "  ${RB}♦️${NC} ${YB}Daily Data Usage    : $ttoday ${NC}"
-echo -e "  ${RB}♦️${NC} ${YB}Monthly Data Usage  : $tmon ${NC}"
+echo -e "  ${RB}♦️${NC} ${YB}Traffic\e[0m       \e[${text}Today      Yesterday        Month   "
+echo -e "  ${RB}♦️${NC} ${YB}Download\e[0m   \e[${text}   $dtoday    $dyest       $dmon   \e[0m"
+echo -e "  ${RB}♦️${NC} ${YB}Upload\e[0m     \e[${text}   $utoday    $uyest       $umon   \e[0m"
+echo -e "  ${RB}♦️${NC} ${YB}Total\e[0m       \e[${text}  $ttoday    $tyest       $tmon  \e[0m "
 echo -e "${BB}————————————————————————————————————————————————————————${NC}"
 echo -e "   ${YB} SSH     VMESS     VLESS     TROJAN     TROJANGO          "
 echo -e "${y}     $ssh        $vmess         $vless         $tr           $trgo         "
